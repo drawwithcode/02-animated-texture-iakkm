b9b1be631476ba7036843b457339f6ee3fb793e8
iakkm <90707822+iakkm@users.noreply.github.com>
2021-10-12 22:30:36 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,49 @@
-function preload() {
-  // put preload code here
-}
+let angle = 0;
+let amt, startColor, newColor;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  rectMode(CENTER);
+  colorMode(HSB);
+  startColor = color(255, 255, 255);
+  newColor = color(random(255), random(255), random(255));
+  amt = 0;
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  background(0);
+
+  for (let i = 50; i < mouseX; i += 50) {
+    for (let j = 50; j < mouseY; j += 50) {
+      noFill();
+      stroke(lerpColor(startColor, newColor, amt));
+      strokeWeight(3);
+
+      let d = dist(mouseX, mouseY, i + width / 2, j + height / 2);
+
+      let r = d / 5;
+
+      push();
+      translate(i, j);
+      rotate(-angle);
+      rect(0, 0, r, r);
+      pop();
+
+      push();
+      translate(i, j);
+      rotate(angle);
+      rect(0, 0, r, r);
+      pop();
+
+      angle += 0.0003;
+
+      amt += 0.01;
+
+      if (amt >= 1) {
+        amt = 0.0;
+        startColor = newColor;
+        newColor = color(random(255), random(255), random(255));
+      }
+    }
+  }
 }
